ee7263c310969f4655c7bc6fb7d48d0f48e78e99
Mark Zietara <mzietara@hotmail.com>
2015-09-10 20:02:19 -0400

    test

---
+++ pe014.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+/*Find the longest collatz sequence up to 
+ n*/
+int longestCollatzSequence(unsigned int n, int[n][2]) {
+
+
+
+
+	return 0;
+}
+
+
+
+int main() {
+
+
+
+	cout << longestCollatzSequence(3, )
+
+	return 0;
+}
+
+
+
+
